158945d462f71aae207910178922f5a414682eba
tuntun <56946732+tuntun0609@users.noreply.github.com>
2022-11-14 21:53:40 +0800

    feat: 增加定时执行功能

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const updateWb = require('./utils/updateWb');
 const getGithubContribution = require('./utils/getGithubContribution');
 const retry = require('./utils/retry');
+const schedule = require('node-schedule');
 
 const getGithubContributionRetry = retry(getGithubContribution);
 
-const init = async () => {
+const main = async () => {
 	try {
 		const data = await getGithubContributionRetry('tuntun0609');
 		if (data) {
@@ -18,4 +19,8 @@ const init = async () => {
 	}
 }
 
-init();
+const init = (rule) => {
+	const job = schedule.scheduleJob(rule, main);
+}
+
+init('0 0 23 * * ?');
