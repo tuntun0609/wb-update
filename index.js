4fd583460dddffec0c95e708e67ada658cd2662c
tuntun <56946732+tuntun0609@users.noreply.github.com>
2022-11-14 15:39:19 +0800

    refactor: 分离getGithubContribution功能

--- index.js
+++ index.js
@@ -1,36 +1,10 @@
-const path = require('path');
-const axios = require('axios');
-const cheerio = require('cheerio');
-const HttpsProxyAgent = require("https-proxy-agent");
-const textToOneLine = require('./utils/textToOneLine');
 const updateWb = require('./utils/updateWb');
-
-const httpsAgent = new HttpsProxyAgent(`http://127.0.0.1:7890`);
-
-const getGithubContribution = async (name, time) => {
-	try {
-		const date = new Date();
-		const today = time ? time : `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
-		const data = await axios.get(`https://github.com/${name}?from=${today}&to=${today}&tab=overview`, {
-			proxy: false,
-			httpsAgent,
-		});
-		const $ = cheerio.load(data.data);
-		const timelineItems = $('.contribution-activity-listing summary span[class~="text-left"]');
-		const res = [today]
-		timelineItems.each(function (i, el) {
-			res.push(textToOneLine($(this).text()));
-		});
-		return res.join('\n');
-	} catch (error) {
-		console.error('getGithubContribution error', error);
-	}
-}
+const getGithubContribution = require('./utils/getGithubContribution');
 
 const init = async () => {
 	try {
 		const data = await getGithubContribution('tuntun0609');
-		// console.log(data);
+		console.log(data);
 		if (data) {
 			const updateMsg = await updateWb(data);
 			console.log(updateMsg?.msg ? updateMsg.msg : updateMsg);
---
+++ utils/getGithubContribution.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const HttpsProxyAgent = require("https-proxy-agent");
+const textToOneLine = require('./textToOneLine');
+
+const httpsAgent = new HttpsProxyAgent(`http://127.0.0.1:7890`);
+
+const getGithubContribution = async (name, time) => {
+	try {
+		const date = new Date();
+		const today = time ? time : `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+		const data = await axios.get(`https://github.com/${name}?from=${today}&to=${today}&tab=overview`, {
+			proxy: false,
+			httpsAgent,
+		});
+		const $ = cheerio.load(data.data);
+		const timelineItems = $('.contribution-activity-listing summary span[class~="text-left"]');
+		const res = [today]
+		timelineItems.each(function (i, el) {
+			res.push(textToOneLine($(this).text()));
+		});
+		return res.join('\n');
+	} catch (error) {
+		console.error('getGithubContribution error', error);
+	}
+}
+
+module.exports = getGithubContribution;
