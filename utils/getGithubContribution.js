29251293a7c150f1d5376640a25ee03eacb23444
tuntun <56946732+tuntun0609@users.noreply.github.com>
2022-11-14 16:37:38 +0800

    fix: 修复默认时间参数不正确的问题

--- utils/getGithubContribution.js
+++ utils/getGithubContribution.js
@@ -7,13 +7,15 @@ const httpsAgent = new HttpsProxyAgent(`http://127.0.0.1:7890`);
 
 const getGithubContribution = async (name, time) => {
 	const date = new Date();
-	const today = time ? time : `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+	const month = `${(date.getMonth() + 1) > 9 ? (date.getMonth() + 1) : '0' + (date.getMonth() + 1)}`;
+	const day = `${date.getDate() > 9 ? date.getDate() : '0' + date.getDate()}`;
+	const today = time ? time : `${date.getFullYear()}-${month}-${day}`;
 	const data = await axios.get(`https://github.com/${name}?from=${today}&to=${today}&tab=overview`, {
 		proxy: false,
 		httpsAgent,
 	});
 	const $ = cheerio.load(data.data);
-	const timelineItems = $('.contribution-activity-listing summary span[class~="text-left"]');
+	const timelineItems = $('.contribution-activity-listing .TimelineItem h4,summary span[class~="text-left"]');
 	const res = [today]
 	timelineItems.each(function (i, el) {
 		res.push(textToOneLine($(this).text()));
